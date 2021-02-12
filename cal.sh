7f45fba1a94ffe07fb3799f5164e0d0f5f6c2127
yogi k <ydkreddy123@gmail.com>
2021-02-12 07:55:45 +0530

    Revert "some changes"
    This reverts commit 979bef7e72db328a265fd30d6170320b666ac28e.

--- cal.sh
+++ cal.sh
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 iam testing the git pull and git fetcfjffljsdfjsfjasdjflssjslkjfjslkjfjslkjfsdljfkjflasdjjflksajflksjfjlksjfjlsdkjfss
 sfjjlkfjjs
 ljflkajfsdksdlkj
 sdllsjlkjs
 alskj
-=======
-iam testing the git pull and git fetch
->>>>>>> parent of 50ff74a... modifited cal.sh
