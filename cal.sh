979bef7e72db328a265fd30d6170320b666ac28e
yogi k <ydkreddy123@gmail.com>
2021-02-12 07:53:53 +0530

    some changes

--- cal.sh
+++ cal.sh
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 iam testing the git pull and git fetcfjffljsdfjsfjasdjflssjslkjfjslkjfjslkjfsdljfkjflasdjjflksajflksjfjlksjfjlsdkjfss
 sfjjlkfjjs
 ljflkajfsdksdlkj
 sdllsjlkjs
 alskj
+=======
+iam testing the git pull and git fetch
+>>>>>>> parent of 50ff74a... modifited cal.sh
